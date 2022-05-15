bb9f30504d8b32e401bd1eabc351885c685fe079
VanaIgr <65824523+VanaIgr@users.noreply.github.com>
2022-05-15 17:05:49 +0300

    move Unit class to a separate file

---
+++ src/Unit.h
@@ -0,0 +1,145 @@
+#pragma once
+
+#include<type_traits>
+
+namespace unit {
+	template<typename... Args>
+	struct TypeList;
+	template<> struct TypeList<> {};
+	using EmptyTypeList = TypeList<>;
+	
+	template<typename H, typename T>
+	struct TypeList<H, T> {
+		using Head = H;
+		using Tail = T;
+	};
+	
+	template<typename...> struct ConstructList { using type = EmptyTypeList; };
+	template<typename H, typename... T> struct ConstructList<H, T...> { using type = TypeList<H, typename ConstructList<T...>::type>; };
+	
+	template<typename Item, typename TypeList> struct Find;
+	template<typename Item, typename Head, typename Tail> struct Find<Item, TypeList<Head, Tail>> 
+		{ static constexpr bool value = std::is_same_v<Item, Head> || Find<Item, Tail>::value; };
+	template<typename Item> struct Find<Item, EmptyTypeList> { static constexpr bool value = false; };
+	
+	//c++ 20 https://en.cppreference.com/w/cpp/types/remove_cvref
+	template< class T > struct remove_cvref { typedef std::remove_cv_t<std::remove_reference_t<T>> type; };
+	template< class T > using remove_cvref_t = typename remove_cvref<T>::type;
+	
+	template<typename T, typename Hierarchy> struct WiderTypesFor{ using type = unit::EmptyTypeList; };
+	  template<typename T, typename HierarchyHead, typename HeadWiderTypes> struct WiderTypesFor<T, unit::TypeList<HierarchyHead, HeadWiderTypes>> {
+		  using type = std::conditional_t<
+			  std::is_same_v<T, HierarchyHead>,
+			  HeadWiderTypes,
+			  typename WiderTypesFor<T, HeadWiderTypes>::type
+		  >;
+	  };
+	
+	template<typename List> struct FirstElement;
+	  template<typename H, typename H2, typename T> struct FirstElement<TypeList<H, TypeList<H2, T>>> { using type = typename FirstElement<TypeList<H2, T>>::type; };
+	  template<typename H                         > struct FirstElement<TypeList<H, EmptyTypeList>  > { using type = H; };
+	
+	/*struct Hierarchy {
+		using hierarchy = typename unit::ConstructList<Chunk, Block, Cube, Fractional>::type;
+				
+		template<typename T> struct UnitInfo { static constexpr /base class value type/ factor = conversion to base unit as a power of 2; }; 
+	};*/
+	
+	template<typename Hierarchy, typename From, typename To> struct CanCast {
+		static constexpr bool value = Find<From, typename Hierarchy::hierarchy>::value && Find<To, typename Hierarchy::hierarchy>::value;
+	};
+	
+	template<typename Hierarchy, typename From, typename To> struct IsCastExplicit {
+		static constexpr bool value = !(Find<To, typename WiderTypesFor<From, typename Hierarchy::hierarchy>::type>::value);
+	};
+	
+	template<typename Hierarchy, typename From, typename To> struct CommonType {
+		using type = std::conditional_t<
+			IsCastExplicit<Hierarchy, From, To>::value,
+			From,
+			To
+		>;
+	};
+	
+	template<typename Hierarchy>
+	using baseType_t = typename FirstElement<typename Hierarchy::hierarchy>::type;
+	
+	template<typename Hierarchy, typename From, typename To> struct Cast {
+		constexpr static To castUnit(From const from) {
+			return To{ static_cast<typename To::value_type>(
+				(static_cast<typename baseType_t<Hierarchy>::value_type>(from.value())
+				<< Hierarchy::template UnitInfo<From>::baseFactor)
+				>> Hierarchy::template UnitInfo<To>::baseFactor 
+			) };
+		}
+	};
+	
+	
+	template<typename Hierarchy, typename From, typename To> struct HaveCommon {
+		using common = typename CommonType<Hierarchy, From, To>::type;
+		static constexpr bool value = CanCast<Hierarchy, From, common>::value && CanCast<Hierarchy, To, common>::value;
+	};
+	
+	
+	template<typename Hierarchy_, typename ID_, typename value_type_>
+	struct Unit {
+		using This = Unit<Hierarchy_, ID_, value_type_>;
+		using Hierarchy = Hierarchy_;
+		using ID = ID_;
+		using value_type = value_type_;	
+	private:
+		value_type value_;
+	public:
+		constexpr Unit() = default;
+		constexpr Unit(value_type const src) : value_(src) {}	
+		
+		template<typename Other, typename = std::enable_if_t<
+			!std::is_same_v<remove_cvref_t<Other>, This> //this constructor is disabled if argument is of type This
+			&& CanCast<Hierarchy, Other, This>::value
+			&& IsCastExplicit<Hierarchy, Other, This>::value
+		>> constexpr explicit Unit(Other const other) : Unit{ Cast<Hierarchy, Other, This>::castUnit(other).value() } {}
+		
+		template<typename Other, typename = std::enable_if_t<
+			!std::is_same_v<remove_cvref_t<Other>, This> //this constructor is disabled if argument is of type This
+			&& CanCast<Hierarchy, This, Other>::value
+			&& !IsCastExplicit<Hierarchy, This, Other>::value
+		>> constexpr operator Other() const {
+			return Cast<Hierarchy, This, Other>::castUnit(*this);
+		}
+		
+		constexpr value_type value() const { return value_; };
+		
+		constexpr friend This operator+(This const c1, This const c2) { return This{value_type(c1.value() + c2.value()) }; }
+		constexpr friend This &operator+=(This &c1, This const c2) { return c1 = c1 + c2; }
+		
+		constexpr friend This operator-(This const c1,This const c2) { return This{value_type(c1.value() - c2.value()) }; }
+		constexpr friend This &operator-=(This &c1, This const c2) { return c1 = c1 - c2; }
+	
+		template<typename Other, typename = std::enable_if_t<HaveCommon<Hierarchy, This, Other>::value>> constexpr friend auto operator+(This const c1, Other const c2) {
+			using common = typename CommonType<Hierarchy, This, Other>::type;
+			
+			return Cast<Hierarchy, This , common>::castUnit(c1)
+				+ Cast<Hierarchy, Other, common>::castUnit(c2);
+		}
+		
+		#define comp(OPERATOR) constexpr friend auto operator##OPERATOR (This const c1, This const c2) { return c1.value() OPERATOR c2.value(); } \
+		\
+		template<typename Other, typename = std::enable_if_t<HaveCommon<Hierarchy, This, Other>::value>> constexpr friend auto operator##OPERATOR (This const c1, Other const c2) { \
+			using common = typename CommonType<Hierarchy, This, Other>::type; \
+			\
+			return Cast<Hierarchy, This , common>::castUnit(c1) OPERATOR Cast<Hierarchy, Other, common>::castUnit(c2); \
+		}
+		
+		#pragma clang diagnostic push
+		#pragma clang diagnostic ignored "-Winvalid-token-paste"
+		comp(<)
+		comp(<=)
+		comp(>)
+		comp(>=)
+		comp(==)
+		comp(!=)
+		#pragma clang diagnostic pop
+		
+		#undef comp
+	};
+}
--- src/Units.h
+++ src/Units.h
@@ -1,11 +1,12 @@
 #pragma once
-
 #include"Misc.h"
 
 #include<limits>
 #include<stdint.h>
 #include<utility>
 #include<stdexcept>
+
+#include"Unit.h"
 
 namespace units {
 	/*note: constexpr is important here because it disallows int and long long overflows and incorrect shifts, so all the constants below are correct*/
@@ -38,117 +39,8 @@ namespace units {
 	constexpr int fracInCubeDim = 1 << fracInCubeDimAsPow2;
 	/*no fracInCubeCount*/
 	
-	namespace {
-		template<typename... Args>
-		struct TypeList;
-		  template<> struct TypeList<> {};
-		  using EmptyTypeList = TypeList<>;
-		
-		  template<typename H, typename T>
-		  struct TypeList<H, T> {
-		  	  using Head = H;
-		  	  using Tail = T;
-		  };
-		
-		template<typename...> struct ConstructList { using type = EmptyTypeList; };
-		template<typename H, typename... T> struct ConstructList<H, T...> { using type = TypeList<H, typename ConstructList<T...>::type>; };
-		
-		template<typename Item, typename TypeList> struct Find;
-		  template<typename Item, typename Head, typename Tail> struct Find<Item, TypeList<Head, Tail>> 
-		      { static constexpr bool value = std::is_same_v<Item, Head> || Find<Item, Tail>::value; };
-		  template<typename Item> struct Find<Item, EmptyTypeList> { static constexpr bool value = false; };
-		
-		//c++ 20 https://en.cppreference.com/w/cpp/types/remove_cvref
-		template< class T > struct remove_cvref { typedef std::remove_cv_t<std::remove_reference_t<T>> type; };
-		template< class T > using remove_cvref_t = typename remove_cvref<T>::type;
-		
-		template<typename From, typename To, typename Void = void> struct CanCast;// { static constexpr bool value; };
-		template<typename From, typename To, typename Void = void> struct IsCastExplicit;// { static constexpr bool value = /should cast From -> To be explicit/; };
-		template<typename From, typename To, typename Void = void> struct CommonType;// { using type = common type between units From and To; };
-		template<typename From, typename To, typename Void = void> struct Cast;// { constexpr static To castUnit(From); };
-		//	typename Void is for forward declaring class templates in anonymous namespace. 
-		//	Maybe there is a better way for declaring but not defining class templates
-		//	that are visible only to the namespace members themselves
-		
-		template<typename From, typename To> struct HaveCommon {
-			using common = typename CommonType<From, To>::type;
-			static constexpr bool value = CanCast<From, common>::value && CanCast<To, common>::value;
-		};
-	}
-	
-	
-	template<typename ID_, typename value_type_, value_type_ min_, value_type_ max_>
-	struct Unit {
-		using This = Unit<ID_, value_type_, min_, max_>;
-		using ID = ID_;
-		using value_type = value_type_;
-		static constexpr value_type min = min_;
-		static constexpr value_type max = max_;		
-	private:
-		value_type value_;
-	public:
-		//constexpr Unit() = default; //0 may be outside of [min; max]
-		constexpr Unit(value_type const src) : value_(src) { if(min <= value_ && value_ <= max); else throw std::logic_error(""); }	
-		
-		template<typename Other, typename = std::enable_if_t<
-			!std::is_same_v<remove_cvref_t<Other>, This> //disable this constructor if argument is this type
-			&& CanCast<Other, This>::value
-			&& IsCastExplicit<Other, This>::value
-		>> constexpr explicit Unit(Other const other) : Unit{ Cast<Other, This>::castUnit(other).value() } {
-			if(min <= value_ && value_ <= max); else throw std::logic_error("");
-		}
-		
-		template<typename Other, typename = std::enable_if_t<
-			!std::is_same_v<remove_cvref_t<Other>, This> //disable this constructor if argument is this type
-			&& CanCast<This, Other>::value
-			&& !IsCastExplicit<This, Other>::value
-		>> constexpr operator Other() const {
-			return Cast<This, Other>::castUnit(*this);
-		}
-		
-		constexpr value_type value() const { return value_; };
-		
-		constexpr friend This operator+(This const c1, This const c2) { return This{value_type(c1.value() + c2.value()) }; }
-		constexpr friend This &operator+=(This &c1, This const c2) { return c1 = c1 + c2; }
-		
-		constexpr friend This operator-(This const c1,This const c2) { return This{value_type(c1.value() - c2.value()) }; }
-		constexpr friend This &operator-=(This &c1, This const c2) { return c1 = c1 - c2; }
 
-		template<typename Other, typename = std::enable_if_t<HaveCommon<This, Other>::value>> constexpr friend auto operator+(This const c1, Other const c2) {
-			using common = typename CommonType<This, Other>::type;
-			
-			return Cast<This , common>::castUnit(c1)
-				 + Cast<Other, common>::castUnit(c2);
-		}
-		
-		#define comp(OPERATOR) constexpr friend bool operator##OPERATOR (This const c1, This const c2) { return c1.value() OPERATOR c2.value(); } \
-		 \
-		template<typename Other, typename = std::enable_if_t<HaveCommon<This, Other>::value>> constexpr friend bool operator##OPERATOR (This const c1, Other const c2) { \
-			using common = typename CommonType<This, Other>::type; \
-			 \
-			return Cast<This , common>::castUnit(c1) OPERATOR Cast<Other, common>::castUnit(c2); \
-		}
-		
-		#pragma clang diagnostic push
-		#pragma clang diagnostic ignored "-Winvalid-token-paste"
-		  comp(<)
-		  comp(<=)
-		  comp(>)
-		  comp(>=)
-		  comp(==)
-		  comp(!=)
-		#pragma clang diagnostic pop
-		
-		#undef comp
-		
-		template<typename T>
-		constexpr static Unit fromAny(T const value) {
-			if(T(min) <= value && value <= T(max)); 
-			else throw std::logic_error("");
-			
-			return Unit(static_cast<value_type>(value));
-		}
-	};
+	struct UnitsHierarchy;
 
 	//units ID
 	struct UnitFractional;
@@ -157,55 +49,24 @@ namespace units {
 	struct UnitCube;
 
 	//units
-	using Fractional = Unit<UnitFractional, long long, std::numeric_limits<long long>::lowest(), std::numeric_limits<long long>::max()>;
-	using Chunk = Unit<UnitChunk, int, (Fractional::min >> fracInChunkDimAsPow2), (Fractional::max >> fracInChunkDimAsPow2)>;
-	using Block = Unit<UnitBlock, long long, (long long)(Chunk::min) * blocksInChunkDim, (long long)(Chunk::max) * blocksInChunkDim>;
-	using Cube = Unit<UnitCube, long long, (long long)(Block::min) * cubesInBlockDim, (long long)(Block::max) * cubesInBlockDim>;
-	
-	//type conversions
-	namespace {
-		using hierarchy = typename ConstructList<Chunk, Block, Cube, Fractional>::type;
-		
-		template<typename T, typename Hierarchy> struct WiderTypesFor{ using type = EmptyTypeList; };
-		  template<typename T, typename HierarchyHead, typename HeadWiderTypes> struct WiderTypesFor<T, TypeList<HierarchyHead, HeadWiderTypes>> {
-			  using type = std::conditional_t<
-				  std::is_same_v<T, HierarchyHead>,
-				  HeadWiderTypes,
-				  typename WiderTypesFor<T, HeadWiderTypes>::type
-			  >;
-		  };
-				
-		template<typename T> struct UnitInfo;/* { static constexpr /base class value type/ factor = conversion to base unit as a power of 2; };*/ 
-		template<> struct UnitInfo<Chunk>      { static constexpr Fractional::value_type factor = fracInChunkDimAsPow2; };
-		template<> struct UnitInfo<Block>      { static constexpr Fractional::value_type factor = fracInBlockDimAsPow2; };
-		template<> struct UnitInfo<Cube>       { static constexpr Fractional::value_type factor = fracInCubeDimAsPow2;  };
-		template<> struct UnitInfo<Fractional> { static constexpr Fractional::value_type factor = 0;                    };
-	}
-	
-	template<typename From, typename To> struct CanCast<From, To, void> {
-		static constexpr bool value = Find<From, hierarchy>::value && Find<To, hierarchy>::value;
-	};
-	
-	template<typename From, typename To> struct IsCastExplicit<From, To, void> {
-		static constexpr bool value = !(Find<To, typename WiderTypesFor<From, hierarchy>::type>::value);
-	};
-	
-	template<typename From, typename To> struct CommonType<From, To, void> {
-		using type = std::conditional_t<
-			IsCastExplicit<From, To>::value,
-			From,
-			To
-		>;
-	};
-	
-	template<typename From, typename To> struct Cast<From, To, void> {
-		constexpr static To castUnit(From const from) {
-			return To::fromAny(
-				(static_cast<Fractional::value_type>(from.value()) << UnitInfo<From>::factor) >> UnitInfo<To>::factor 
-			);
-		}
-	};
-	
+	using Fractional = unit::Unit<UnitsHierarchy, UnitFractional, long long>;
+	using Chunk      = unit::Unit<UnitsHierarchy, UnitChunk     , int>;
+	using Block      = unit::Unit<UnitsHierarchy, UnitBlock     , int>;
+	using Cube       = unit::Unit<UnitsHierarchy, UnitCube      , int>;
+}
+
+//type conversions
+struct units::UnitsHierarchy {	
+	using hierarchy = typename unit::ConstructList<Chunk, Block, Cube, Fractional>::type;
+			
+	template<typename T> struct UnitInfo;
+	template<> struct UnitInfo<Chunk>      { static constexpr Fractional::value_type baseFactor = fracInChunkDimAsPow2; };
+	template<> struct UnitInfo<Block>      { static constexpr Fractional::value_type baseFactor = fracInBlockDimAsPow2; };
+	template<> struct UnitInfo<Cube>       { static constexpr Fractional::value_type baseFactor = fracInCubeDimAsPow2;  };
+	template<> struct UnitInfo<Fractional> { static constexpr Fractional::value_type baseFactor = 0;                    };
+};
+
+namespace units {
 	//helpers
 	template<typename Unit, typename Other> constexpr bool fitsIn(Unit const unit, Other const other) {
 		return other <= unit && unit < (other + 1);
